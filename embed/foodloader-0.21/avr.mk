a06ae5d05fe432095c5e1ac3c6b22a9a36c88494
Joerg Bornschein <jb@capsec.org>
2010-06-21 11:44:28 +0200

    Make the bootloader compile on Ubuntu 10.4

--- embed/foodloader-0.21/avr.mk
+++ embed/foodloader-0.21/avr.mk
@@ -50,7 +50,7 @@ ifneq ($(DEBUG),)
 	CFLAGS += -Wmissing-declarations -Wredundant-decls
 	CFLAGS += -Wstrict-prototypes -Wshadow -Wbad-function-cast
 	CFLAGS += -Winline -Wpointer-arith -Wsign-compare
-	CFLAGS += -Wunreachable-code -Wdisabled-optimization -Werror
+	CFLAGS += -Wunreachable-code -Wdisabled-optimization #-Werror
 	CFLAGS += -Wcast-align -Wwrite-strings -Wnested-externs -Wundef
 	CFLAGS += -Wa,-adhlns=$(basename $@).lst
 	CFLAGS += -DDEBUG
